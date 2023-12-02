a3cca303ba86217f255ca5cac5accd91cb299bb9
Guru Raj <gururaj@Gurus-MacBook-Air.local>
2023-12-02 09:43:49 +0530

    first commit

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const cors = require("cors");
 const service = require("./routes/services");
 //const { mongoose_connect } = require("./mongodb/connect");
 const agentroutes = require("./routes/Agentproduct");
+var responseTime = require("response-time");
 
 const app = express();
 //mongoose_connect();
 app.use(cors());
 
 app.use(express.json());
+app.use(
+  responseTime((req, res, time) => {
+    console.log(req.method, req.url, time + "ms");
+  })
+);
 
 // Health check route
 app.get("/health", (req, res) => {
